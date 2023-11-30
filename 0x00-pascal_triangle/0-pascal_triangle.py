9ab1b8908a604c134027c2723c97c23f2294d3df
raymond <stancillousray@gmail.com>
2023-11-30 23:26:58 +0300

    pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
+    """
+    function that returns a list of lists of integers
+    representing the pascal's triangle
+    """
 
     if n <= 0:
         return []
--- 0x00-pascal_triangle/new.py
+++ 0x00-pascal_triangle/new.py
@@ -9,4 +9,4 @@ def print_triangle(triangle):
 
 
 if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
+    print_triangle(pascal_triangle(1))
