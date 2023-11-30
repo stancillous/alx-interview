eb42599e5da64cbea160abfd10ae8adedb0a8bc8
raymond <stancillousray@gmail.com>
2023-11-30 23:16:50 +0300

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+def pascal_triangle(n):
+
+    if n <= 0:
+        return []
+
+    final_list = []  # is an array of arrays
+    topArray = [1]
+    for i in range(n):
+        final_list.append(topArray)
+        lenTop = len(topArray)
+
+        newArray = []
+        newArray.append(1)  # first element
+        newArray[-1] = 1
+
+        for j in range(1, lenTop):
+            num = topArray[j - 1] + topArray[j]
+            newArray.append(num)
+        newArray.append(1)  # last element
+        topArray = newArray
+    return final_list
