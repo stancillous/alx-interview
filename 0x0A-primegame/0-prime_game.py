0cd682ae5ad50ca88e00e9ad60306f162521741d
raymond <stancillousray@gmail.com>
2024-02-15 12:01:52 +0300

    fix my isPrime function

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,7 +9,7 @@ def isPrime(num: int):
     """determines if a number is Prime"""
     if num < 2:
         return False
-    for i in range(2, int(num**0.5) + 1):
+    for i in range(2, num):
         if num % i == 0:
             return False
     return True
